2d0b3b8f439269f4f50393b49f46cda3efbc769b
frkht <frkhit@gmail.com>
2019-12-03 18:41:02 +0800

    spark metrics: graphite

--- crimes-app-collect-log.sh
+++ crimes-app-collect-log.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --rm --network docker-spark-cluster_spark-network -v `pwd`/data/spark-apps:/opt/spark-apps --env PYSPARK_PYTHON=python3 frkhit/docker-spark-cluster-submit:latest /spark/bin/spark-submit /opt/spark-apps/collect_log.py
+docker run --rm --network docker-spark-cluster_spark-network -v `pwd`/data/spark-apps:/opt/spark-apps -v `pwd`/data/spark-data:/opt/spark-data --env PYSPARK_PYTHON=python3 frkhit/docker-spark-cluster-submit:latest /spark/bin/spark-submit --conf spark.metrics.conf='/opt/spark-data/metrics.properties' /opt/spark-apps/collect_log.py
