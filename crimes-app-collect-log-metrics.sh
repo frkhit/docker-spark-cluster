975fde3db1c8de095d5d43c86504fc59b44cb050
frkht <frkhit@gmail.com>
2019-12-04 09:31:56 +0800

    metrics

---
+++ crimes-app-collect-log-metrics.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker run --rm --network docker-spark-cluster_spark-network -v `pwd`/data/spark-apps:/opt/spark-apps -v `pwd`/data/spark-data:/opt/spark-data --env PYSPARK_PYTHON=python3 frkhit/docker-spark-cluster-submit:latest /spark/bin/spark-submit --conf spark.metrics.conf='/opt/spark-data/metrics.properties' /opt/spark-apps/collect_log_metrics.py
